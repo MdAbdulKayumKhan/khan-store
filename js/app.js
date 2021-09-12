0f5c2e612edcee9ba0121057743fee867b0f2b46
MdAbdulKayumKhan <kayum261@gmail.com>
2021-09-13 03:04:11 +0600

    button design by bootstrap 5

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h4>Rating:<strong> ${product.rating.rate}</strong> Persons: <strong> ${product.rating.count}</strong> </h4>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
